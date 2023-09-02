c84b6f01a414ceeb71321e22d33e49663e2d40a5
Liam Robertson <liam2robertson@gmail.com>
2023-09-02 23:54:02 +0100

    fix fonts wrongly being exported from layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import '../styles/globals.scss'
 import type { Metadata } from 'next'
 import { Rubik, Roboto_Mono } from 'next/font/google'
 
-export const rubik = Rubik({ subsets: ['latin'], display: 'swap', variable: '--font-rubik' })
-export const robotoMono = Roboto_Mono({ subsets: ['latin'], display: 'swap', variable: '--font-roboto-mono' })
+const rubik = Rubik({ subsets: ['latin'], display: 'swap', variable: '--font-rubik' })
+const robotoMono = Roboto_Mono({ subsets: ['latin'], display: 'swap', variable: '--font-roboto-mono' })
 
 export const metadata: Metadata = {
   title: 'Interactive YAML tutorial',
